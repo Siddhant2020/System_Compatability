52b4d04f3f67c8f18fede5ad4ee536d1e020f02b
Siddhant Ashok <siddhant.ashok091994@gmail.com>
2021-12-18 13:20:03 +0530

    Initial Commit

---
+++ SystemCompatability/SystemCompatability/CompatabilityClass/Compatability.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemCompatability.CompatabilityClass
+{
+    public class Compatability
+    {
+        //Om Ganeshaye Namah
+
+        private static async Task ListAllCamerasAsync()
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM win32_PNPEntity WHERE PnPClass='Camera'");
+            var collection = await Task.Run(() => { return searcher.Get(); });
+            var cameraIndex = 0;
+            foreach (ManagementObject cameraDevice in collection)
+            {
+                Console.WriteLine($"Camera {cameraIndex}: {cameraDevice["Name"]}");
+                cameraIndex++;
+            }
+        }
+    }
+}
---
+++ SystemCompatability/SystemCompatibilityConsole/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Management;
+using System.Threading.Tasks;
+
+namespace SystemCompatibilityConsole
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Program p = new Program();
+                p.ListAllCamerasAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Camera Detection Exception" + ex.Message + ex.StackTrace);
+            }
+        }
+
+        public void ListAllCamerasAsync()
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM win32_PNPEntity WHERE PnPClass='Camera'");
+            var collection = Task.Run(() => { return searcher.Get(); }).Result;
+            var cameraIndex = 0;
+            foreach (ManagementObject cameraDevice in collection)
+            {
+                Console.WriteLine($"Camera {cameraIndex}: {cameraDevice["Name"]}");
+                cameraIndex++;
+            }
+        }
+    }
+}
