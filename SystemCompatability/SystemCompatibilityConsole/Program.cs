d7782c63a6b542a48ef1e91e6ba373495a9c30a4
Siddhant Ashok <siddhant.ashok091994@gmail.com>
2021-12-18 16:49:12 +0530

    Webcam(Yet to enhance) and Network Stability test

--- SystemCompatability/SystemCompatibilityConsole/Program.cs
+++ SystemCompatability/SystemCompatibilityConsole/Program.cs
@@ -1,17 +1,41 @@
 ﻿using System;
+using System.Globalization;
 using System.Management;
+using System.Net;
+using System.Net.NetworkInformation;
 using System.Threading.Tasks;
 
 namespace SystemCompatibilityConsole
 {
     public class Program
     {
+        //Om Ganeshaye Namah
         static void Main(string[] args)
         {
             try
             {
-                Program p = new Program();
-                p.ListAllCamerasAsync();
+                bool isCameradetected = ListAllCamerasAsync();
+
+                if (isCameradetected)
+                {
+                    Console.WriteLine("-------------- Camera Detected. -------------------");
+                }
+                else
+                {
+                    Console.WriteLine("------------------ Couldn't Detected any Camera. -------------");
+                }
+
+
+                bool isStableConneciton = CheckForInternetConnection();
+
+                if (isStableConneciton)
+                {
+                    Console.WriteLine("------------ Stable Conneciton -----------");
+                }
+                else
+                {
+                    Console.WriteLine("----------- Unstable Connection ----------");
+                }
             }
             catch (Exception ex)
             {
@@ -19,7 +43,7 @@ namespace SystemCompatibilityConsole
             }
         }
 
-        public void ListAllCamerasAsync()
+        private static bool ListAllCamerasAsync()
         {
             ManagementObjectSearcher searcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM win32_PNPEntity WHERE PnPClass='Camera'");
             var collection = Task.Run(() => { return searcher.Get(); }).Result;
@@ -29,6 +53,58 @@ namespace SystemCompatibilityConsole
                 Console.WriteLine($"Camera {cameraIndex}: {cameraDevice["Name"]}");
                 cameraIndex++;
             }
+
+            if (cameraIndex > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
+
+        public static bool CheckForInternetConnection(int timeoutMs = 10000, string url = null)
+        {
+            //try
+            //{
+            //    url ??= CultureInfo.InstalledUICulture switch
+            //    {
+            //        { Name: var n } when n.StartsWith("fa") => // Iran
+            //            "http://www.aparat.com",
+            //        { Name: var n } when n.StartsWith("zh") => // China
+            //            "http://www.baidu.com",
+            //        _ =>
+            //            "http://www.gstatic.com/generate_204",
+            //    };
+
+            //    var request = (HttpWebRequest)WebRequest.Create(url);
+            //    request.KeepAlive = false;
+            //    request.Timeout = timeoutMs;
+            //    using (var response = (HttpWebResponse)request.GetResponse())
+            //        return true;
+            //}
+            //catch
+            //{
+            //    return false;
+            //}
+
+            try
+            {
+                Ping myPing = new Ping();
+                String host = "google.com";
+                byte[] buffer = new byte[32];
+                int timeout = 1000;
+                PingOptions pingOptions = new PingOptions();
+                PingReply reply = myPing.Send(host, timeout, buffer, pingOptions);
+                return (reply.Status == IPStatus.Success);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+
     }
 }
